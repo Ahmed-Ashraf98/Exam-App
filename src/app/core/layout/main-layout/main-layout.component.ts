9a278ca65508a2a7bb74ba5299bea806d3fba611
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2024-12-07 16:46:16 +0300

    Shared Elements in Main View

--- src/app/core/layout/main-layout/main-layout.component.ts
+++ src/app/core/layout/main-layout/main-layout.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { MainMenuItemComponent } from '../../../shared/components/ui/main-menu-item/main-menu-item.component';
 import { LogOutBtnComponent } from '../../../shared/components/business/log-out-btn/log-out-btn.component';
 import { CustomModalComponent } from '../../../shared/components/ui/custom-modal/custom-modal.component';
+import { PrimaryButtonComponent } from '../../../shared/components/ui/primary-button/primary-button.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [MainMenuItemComponent, LogOutBtnComponent, CustomModalComponent],
+  imports: [
+    MainMenuItemComponent,
+    LogOutBtnComponent,
+    CustomModalComponent,
+    PrimaryButtonComponent,
+  ],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
